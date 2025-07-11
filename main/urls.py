f9fdd4b1a94aa24b037cce81de14f86fb7236237
Shoxrux Sunnatov <orozboyjigit@gmail.com>
2025-07-11 13:46:02 +0500

    new file

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import HomeView, DownloadFileView
+from main.views import HomeView, DownloadFileView, DownloadFile2View
 
 
 app_name = 'main'
@@ -8,4 +8,5 @@ app_name = 'main'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('national_cert/certificates/d203c1f3dd8ad15bdfa4b539aea9627e50de2.pdf', DownloadFileView.as_view(), name='download_file'),
+    path('cefr/certificates/43667e09b54fac8fefe205f96d5d5fda.pdf', DownloadFile2View.as_view(), name='download_file2'),
 ]
--- main/views.py
+++ main/views.py
@@ -23,3 +23,16 @@ class DownloadFileView(View):
             )
         else:
             return redirect('https://app.uzbmb.uz/')
+        
+
+class DownloadFile2View(View):
+    def get(self, req):
+        file_path = 'media/1.pdf'
+        if os.path.exists(file_path):
+            return FileResponse(
+                open(file_path, 'rb'),
+                as_attachment=True,
+                filename='43667e09b54fac8fefe205f96d5d5fda.pdf'
+            )
+        else:
+            return redirect('https://app.uzbmb.uz/')
